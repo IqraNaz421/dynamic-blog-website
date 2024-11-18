9d4c4c389810120ff320a05c0547fba69d2bdf86
IqraNaz421 <161621730+IqraNaz421@users.noreply.github.com>
2024-11-18 22:42:49 +0500

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,59 @@
-import Image from "next/image";
 
-export default function Home() {
+
+import './globals.css';
+import Hero from '@/components/Hero';
+import Link from 'next/link';
+import Image from 'next/image';
+import About from '@/components/About';
+
+const Home = () => {
+  const posts = [
+    { id: '1', title: 'Ice Cream Website', date: 'Nov 17, 2024', excerpt: 'Ice cream is more than just a dessert—its an experience! In this project I delved into the deliciously creative world of ice cream crafting a design that reflects the cool and refreshing vibes of everyones favorite treat', image: '/images/ice cream.jpg' },
+
+    { id: '2', title: 'Baby Baba Website', date: 'Nov 18, 2024', excerpt: 'Welcome to Baby Baba Shop the ultimate destination for stylish comfortable and high-quality baby clothing! 👶✨ Whether you are shopping for a newborn or a trendy toddler this e-commerce platform', image: '/images/baby.jpeg' },
+
+    { id: '3', title: 'Perfume Website', date: 'Nov 19, 2024', excerpt: 'Welcome to Aroma Essence your ultimate destination for captivating fragrances that define elegance and sophistication 🌸✨ Discover a world of premium perfumes style and leave a lasting impression 💫', image: '/images/perfume.jpeg' },
+
+    { id: '4', title: 'Blog Website', date: 'Nov 20, 2024', excerpt: 'Welcome to My Blog Website 📝✨ where creativity meets expression! Dive into a world of insightful articles personal stories, and engaging content all crafted to inspire and inform 🌟📚', image: '/images/blog.jpg' },
+
+    { id: '5', title: 'Resume Builder', date: 'Nov 21, 2024', excerpt: 'Introducing the Resume Builder 💼✨—your personalized tool to create a standout resume with ease! Simply input your details and watch your professional profile come to life in minutes! 📝🚀', image: '/images/builder.png' },
+
+    { id: '6', title: 'Cloths Website', date: 'Nov 22, 2024', excerpt: 'Welcome to StyleSphere 👗✨ your one-stop destination for trendy and affordable fashion! Discover the latest styles and elevate your wardrobe with ease 🛍️💃', image: '/images/cloth.webp' },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-black text-white min-h-screen">
+      <Hero />
+      {/* <AboutCard/> */}
+      <main className="max-w-6xl mx-auto p-6">
+        <section id="home">
+          <h2 className="text-4xl font-bold text-center text-white mb-12">Latest Posts</h2>
+          {/* Adjusting flexbox layout for responsiveness */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {posts.map((post) => (
+              <div key={post.id} className="bg-black border-2 border-blue-600 text-white p-6 shadow-lg rounded-lg transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:translate-y-2 w-full  hover:border-blue-900 ">
+                {/* Post Image */}
+                <div className="mb-4">
+                  <Image src={post.image} alt={post.title} width={500} height={500} className="w-full border-2 border-pink-500 h-48 object-cover rounded-lg shadow-md" />
+                </div>
+                <h2 className="text-2xl font-semibold mb-2">{post.title}</h2>
+                <p className="text-sm text-teal-100 mb-4">{post.date}</p>
+                <p className="text-gray-200 mb-4">{post.excerpt}</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+              
+
+                {/* Read More Link */}
+                <Link href={`/posts/${post.id}`} className="text-blue-500 hover:text-pink-500 font-semibold transition duration-300">
+                  Read More...
+                </Link>
+              </div>
+            ))}
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <About/>
     </div>
   );
-}
+};
+
+export default Home;
